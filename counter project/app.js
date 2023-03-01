dda02f423e38f6b58dd4d9149ff1853db7f519d1
Vashu kashyap <104416213+vashu-kashyap@users.noreply.github.com>
2023-03-01 22:28:43 +0530

    Add files via upload

---
+++ counter project/app.js
@@ -0,0 +1,34 @@
+let count = 0;
+
+// select value and all btns
+
+const value = document.querySelector('#value')
+const btns = document.querySelectorAll('.btn')
+
+btns.forEach(function(btn){
+
+    btn.addEventListener('click', function(e){
+       const styles =  e.currentTarget.classList
+
+       if (styles.contains('decrease')) {
+
+            count--;
+
+       } else if (styles.contains('increase')){
+            count++;
+       } else{
+            count = 0
+       }
+
+       if (count > 0) {
+            value.style.color = 'Green'
+       } else if (count < 0){
+            value.style.color = 'red'
+       } else{
+            value.style.color = 'white'        
+    }
+
+       value.textContent = count;
+    })
+
+})
